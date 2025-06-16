b83083b4b38b99aeecec5e52ece19043f8261b61
jabuticaba123 <165790806+jabuticaba123@users.noreply.github.com>
2025-06-16 10:58:08 -0300

    h

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Ao acessar sites que a uma lista com diversas escolhas de carreira. Você decide selecionar opções que vão de acordo com suas habilidades.",
         alternativas: [
             {
                 texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
